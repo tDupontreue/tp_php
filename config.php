716cbe8031765ab9428225f8dc198f4ccaf86dfe
FIRST_NAME LAST_NAME <dupontreuetheo21@gmail.com>
2022-04-28 16:03:37 +0200

    commit

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
     try 
     {
         //$bdd = new PDO("mysql:host=localhost;dbname=cooking;charset=utf8", "root", "");
-        $bdd = new PDO("mysql:host=localhost;dbname=cooking;charset=utf8", "root", "");
+        $bdd = new PDO("mysql:host=192.168.65.54;dbname=cooking;charset=utf8", "root", "");
     }
     catch(PDOException $e)
     {
